f219a8b29e320b6a78ceee41287fab100a7c91da
root <root@201011JUNC0434.(none)>
2015-03-05 15:22:06 +0530

    changed the favourite colour

--- string.rb
+++ string.rb
@@ -1,4 +1,4 @@
 first_name="sundar"
 last_name="virat"
-favourite_colour="red"
+favourite_colour="violet"
 print "hi my name" +first_name+ "" +last_name+"and my favourite colour is"+favourite_colour+"\n"
